5af1a9a4e13bc905318a9515f52e1c24a61beefc
Matt Patterson <matt@reprocessed.org>
2011-05-14 21:23:11 +0300

    Extract date fields; do less harm than good...

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 Unmangles the Ancient Petitions CSV by:
 * recombining long fields split across several fixed length fields
+
+Run with:
+
+    $ ruby ancient_petitions_unmangler.rb /path/to/ancient_petitions.csv
+
--- ancient_petitions_unmangler.rb
+++ ancient_petitions_unmangler.rb
@@ -8,18 +8,52 @@ raise "input file doesn't exist" unless File.file?(input_path)
 output_path = File.join(File.dirname(input_path), (File.basename(input_path, '.csv') + '-unmangled.csv')) 
 
 input_csv = CSV.open(input_path, 'r:windows-1252', :headers => true, :return_headers => true)
-out = CSV.open(output_path, 'wb:windows-1252', :headers => input_csv.shift.headers)
-processed_rows = 0
+input_headers = input_csv.shift.headers
+input_headers.delete('scopeoflow') 
+input_headers = input_headers + ['date_from', 'date_to']
+out = CSV.open(output_path, 'wb:windows-1252')
 
-def consolidate_docscope(row)
-  row['docsope'] = row['docscope'] + row['scopeoflow'] unless row['scopeoflow'].nil?
-  row.delete('scopeoflow')
+def consolidate_docscope(row_hash)
+  row_hash['docscope'] = (row_hash['docscope'] + row_hash['scopeoflow']) unless row_hash['scopeoflow'].nil?
 end
 
+def extract_date_fields(row_hash)
+  # [c YYYY - c YYYY]
+  # [c YYYY - c]
+  # [c YYYY - c YYYY]
+  # [YYYY - YYYY]
+  # [YYYY]
+  # YYYY
+  
+  date_field = row_hash['docdate']
+  if date_field.nil?
+    start_date = end_date = nil
+  else
+    date_match = date_field.match(/[^0-9]*([0-9]{4})(?:[^0-9]+([0-9]{4}))?/)
+    start_date, end_date = date_match.nil? ? [nil, nil] : date_match.captures
+  end
+  row_hash['date_from'] = start_date
+  row_hash['date_to'] = end_date
+end
+
+def construct_row(headers, row_hash)
+  headers.collect { |k| row_hash[k] }
+end
+
+@processed_rows = 0
+def update_progress
+  @processed_rows += 1
+  $stdout.print "." if @processed_rows % 100 == 0
+end
+
+out << input_headers
 while row = input_csv.shift
-  consolidate_docsope(row)
+  row_hash = row.to_hash
+  consolidate_docscope(row_hash)
+  extract_date_fields(row_hash)
 
-  out << row
-  processed_rows += 1
-  print "." if processed_rows % 100 == 0
+  out << construct_row(input_headers, row_hash)
+
+  update_progress
 end
+puts ""
